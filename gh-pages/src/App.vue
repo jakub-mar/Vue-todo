655a5adc7ff04542b126db2b7b0dcbf7aa93f3fc
Jakub Marciniak <jakub.marciniak2000@gmail.com>
2022-07-20 22:55:15 +0200

    Merge commit '0beb55a11cf67b0b74121f1f3eb2b67afb3ab6d5' as 'gh-pages'

---
+++ gh-pages/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+<h1>Todo app</h1>
+<TodoApp />
+</div>
+</template>
+
+<script>
+import Todo from './components/todoApp.vue'
+
+export default{
+  components:{
+    TodoApp:Todo
+  }
+}
+
+</script>
